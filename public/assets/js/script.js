d6216cff9b551cb95d5a2dbf5e4ca582cc19f56f
will <wsherrill17@gmail.com>
2019-09-25 09:04:18 -0400

    burgers are showing on page now need devour

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,11 +9,17 @@ $(document).ready(function() {
       var burger =  $('#burgerToGo').val()
       console.log (burger)
       $.post("/api/burgers", {burger_name: burger}, function(data, status){
-        console.log('DAta sent to the burgers post', data)
-        alert("Data: " + data + "\nStatus: " + status);
+        window.location.replace("/")
       }).catch(error => console.log('error from failed put', error));
     })
-    
+    $('#get').on("click", function(event){
+      event.preventDefault()
+    var burger =  $('#burgerToGo').val()
+    console.log (burger)
+    $.post("/api/burgers/:id", {burger_name: burger}, function(data, status){
+      window.location.replace("/")
+    }).catch(error => console.log('error from failed put', error));
+  })
 })
 
 
